3f7bc6e92f3cfc08fec99b416fb882c1262f0c78
balukin <balukin@gmail.com>
2025-01-07 17:08:04 +0100

    basic HUD explainer text

--- src/sandboxrust/Code/Atmosphere.cs
+++ src/sandboxrust/Code/Atmosphere.cs
@@ -11,4 +11,38 @@ public sealed class Atmosphere : Component
 
     [Property, Range(0, 1)]
     public float WaterVapor { get; set; } = 0.5f;
+
+    protected override void OnUpdate()
+    {
+        HandleInput();
+        CameraHud.Current.AtmosphereText = $"Oxygen Level: {OxygenLevel:P0} (affects rusting speed, adjust with Arrow keys)";
+    }
+
+    private void HandleInput()
+    {
+        var minorStep = 0.01f;
+        var majorStep = 0.1f;
+
+        if (Input.Pressed("o2_minor_step_down"))
+        {
+            OxygenLevel -= minorStep;
+        }
+        if (Input.Pressed("o2_minor_step_up"))
+        {
+            OxygenLevel += minorStep;
+        }
+        if (Input.Pressed("o2_major_step_up"))
+        {
+            OxygenLevel += majorStep;
+        }
+        if (Input.Pressed("o2_major_step_down"))
+        {
+            OxygenLevel -= majorStep;
+        }
+
+        if (OxygenLevel < 0)
+        {
+            OxygenLevel = 0;
+        }
+    }
 } 
---
+++ src/sandboxrust/Code/CameraHud.cs
@@ -0,0 +1,37 @@
+using Sandbox;
+using Sandbox.Rendering;
+
+public sealed class CameraHud : Component
+{
+	private CameraComponent cam;
+	public static CameraHud Current { get; private set; }
+	public string AtmosphereText { get; set; }
+	private int topLeftCurrentLineOffset = 0;
+	private const int lineHeight = 20;
+
+	protected override void OnStart()
+	{
+		cam = GetComponent<CameraComponent>();
+		Current = this;
+	}
+	protected override void OnUpdate()
+	{
+		var hud = cam.Hud;
+		WriteExplainers(hud);
+	}
+
+
+
+	private void WriteExplainers(HudPainter hud)
+	{
+		topLeftCurrentLineOffset = 0;
+		WriteLine(hud, "Hello!");
+		WriteLine(hud, AtmosphereText);
+	}
+
+	private void WriteLine(HudPainter hud, string text)
+	{
+		hud.DrawText(new TextRendering.Scope(text, Color.Red, lineHeight), new Vector2(10, 10 + topLeftCurrentLineOffset), TextFlag.LeftTop);
+		topLeftCurrentLineOffset += lineHeight;
+	}
+}
