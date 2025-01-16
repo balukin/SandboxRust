55dfd1464643ee9cc882c248c553c1f7a79009d7
balukin <balukin@gmail.com>
2025-01-16 21:22:41 +0100

    restore accidentally gitignored debug helpers

---
+++ src/sandboxrust/Code/Testing/DebugObject.cs
@@ -0,0 +1,84 @@
+
+
+/// <summary>
+/// Simple debug object that can be created when something needs to be visually checked.
+/// </summary>
+public class DebugObject : Component
+{
+    public float timeout = -1;
+    private TimeSince timeSinceCreated;
+    public Vector3? direction;
+
+    public static DebugObjectBuilder Create()
+    {
+        return new DebugObjectBuilder();
+    }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+        timeSinceCreated = 0;
+    }
+
+    protected override void OnUpdate()
+    {
+        if ( timeout > 0 && timeSinceCreated > timeout )
+        {
+            GameObject.Destroy();
+        }
+
+        if ( direction.HasValue )
+        {
+            var target = WorldPosition + direction.Value * 100;
+            Gizmo.Draw.Arrow( WorldPosition, target, 10, 2 );
+        }
+        else
+        {
+            Gizmo.Draw.SolidSphere( WorldPosition, 5 );
+        }
+    }
+
+    public class DebugObjectBuilder
+    {
+        private GameObject debugObject;
+        private DebugObject component;
+
+        public DebugObjectBuilder()
+        {
+            debugObject = new GameObject();
+            component = debugObject.AddComponent<DebugObject>();
+        }
+
+        public DebugObjectBuilder WithName( string name )
+        {
+            debugObject.Name = name;
+            return this;
+        }
+
+        public DebugObjectBuilder WithPosition( Vector3 position )
+        {
+            debugObject.WorldPosition = position;
+            return this;
+        }
+
+        public DebugObjectBuilder WithTimeout( float seconds )
+        {
+            component.timeout = seconds;
+            return this;
+        }
+
+        public DebugObjectBuilder WithDirection( Vector3 direction )
+        {
+            component.direction = direction;
+            return this;
+        }
+
+        public DebugObject Get()
+        {
+            return component;
+        }
+    }
+
+
+}
+
---
+++ src/sandboxrust/Code/Testing/TestMovement.cs
@@ -0,0 +1,29 @@
+using System;
+using Sandbox;
+
+/// <summary>
+/// Test component that makes objects move in a simple pattern.
+/// Useful for verifying coordinate mappings and shader behaviors.
+/// </summary>
+public sealed class TestMovement : Component
+{
+    /// <summary>
+    /// If true, the object will rotate around the up axis.
+    /// </summary>
+    [Property]
+    public bool IsRotating { get; set; }
+
+    [Property]
+    public float RotationSpeedDegPerSec { get; set; } = 30f;
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+
+        if ( IsRotating )
+        {
+            var rotation = Rotation.FromAxis( Vector3.Up, Time.Delta * RotationSpeedDegPerSec );
+            Transform.Local = Transform.Local.RotateAround( Transform.Local.Position, rotation );
+        }
+    }
+}
