0f27e6cf215efbba1115a4ab617872d9a98d9f6a
balukin <balukin@gmail.com>
2025-01-16 03:23:21 +0100

    added water spray sound (white noise)

--- src/sandboxrust/Code/SprayParticleSystem.cs
+++ src/sandboxrust/Code/SprayParticleSystem.cs
@@ -1,12 +1,30 @@
+using System;
+
 public class SprayParticleSystem : Component
 {
-    
+
     [Property]
     public ParticleConeEmitter Emitter;
 
-    
-    public float Rate 
+    [Property]
+    public SoundPointComponent Sound;
+
+    private float lastRate = 0;
+    public float Rate
     {
-        set => Emitter.Rate = value;
+        set
+        {
+            if ( lastRate == value )
+            {
+                return;
+            }
+
+
+            Emitter.Rate = value;
+            Sound.Pitch = Random.Shared.Next( 50, 150 ) / 100f;
+            Sound.Volume = value > 0 ? Random.Shared.Next( 25, 30 ) / 100f : 0;
+
+            lastRate = value;
+        }
     }
 }
